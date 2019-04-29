3c813e8477f58478a660c0410fcc85c66393f8fa
Dantas <xp1998@live.com.pt>
2019-04-29 23:52:56 +0100

    classes..algumas

---
+++ SweetChef/SweetChef/Models/CozinhadoContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetChef.Models
+{
+    public class CozinhadoContext : DbContext
+    {
+        public DbSet<Receita> Receitas { set; get; }
+        public DbSet<Ingrediente> Ingredientes { set; get; }
+        public DbSet<Passo> Passos { set; get; }
+
+        public CozinhadoContext(DbContextOptions<CozinhadoContext> options)
+              : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //--------------------------N-N 
+            modelBuilder.Entity<ReceitaIngrediente>()
+                .HasKey(t => new { t.ReceitaId, t.IngredienteId });
+
+            modelBuilder.Entity<ReceitaIngrediente>()
+                .HasOne(pt => pt.Receita)
+                .WithMany(p => p.ReceitaIngredientes)
+                .HasForeignKey(pt => pt.ReceitaId);
+
+            modelBuilder.Entity<ReceitaIngrediente>()
+                .HasOne(pt => pt.Ingrediente)
+                .WithMany(t => t.ReceitaIngredientes)
+                .HasForeignKey(pt => pt.IngredienteId);
+
+            //-------------------------N-N
+
+            modelBuilder.Entity<PassoIngrediente>()
+                .HasKey(t => new { t.PassoId, t.IngredienteId });
+
+            modelBuilder.Entity<PassoIngrediente>()
+                .HasOne(pt => pt.Passo)
+                .WithMany(p => p.PassoIngredientes)
+                .HasForeignKey(pt => pt.PassoId);
+
+            modelBuilder.Entity<PassoIngrediente>()
+                .HasOne(pt => pt.Ingrediente)
+                .WithMany(t => t.PassoIngredientes)
+                .HasForeignKey(pt => pt.IngredienteId);
+
+            //------------------------N-1
+
+            modelBuilder.Entity<Passo>()
+                .HasOne(t => t.Receita)
+                .WithMany(u => u.Passos)
+                .HasForeignKey(t => t.ReceitaId);
+                //.HasConstraintName("ForeignKey_Receita_Passo");
+        }
+    }
+}
---
+++ SweetChef/SweetChef/Models/Receita.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetChef.Models
+{
+    public class Receita
+    {   
+        [Key]
+        public int Id { set; get; }
+        [Required]
+        public string Nutricao { set; get; }
+        [Required]
+        public string ImageLink { set; get; }
+        [Required]
+        public string Descricao { set; get; }
+        [Required]
+        public string Nome { set; get; }
+        [Required]
+        public int Dificuldade { set; get; }
+        [Required]
+        public int Porcoes { set; get; }
+        [Required]
+        public TimeSpan TempoDePreparacao { set; get; }
+        [Required]
+        public TimeSpan TempoDeEspera { set; get; }
+
+        public List<ReceitaIngrediente> ReceitaIngredientes { set; get; }
+
+        public List<Passo> Passos { set; get; }
+
+    }
+}
