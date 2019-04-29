695a570eda03c7572e5476b31a84b74220d54a5c
Daniel Costa <a81302@alunos.uminho.pt>
2019-04-29 15:38:09 +0100

    Adiciona contexto ao controlador do Utilizador

---
+++ SweetChef/SweetChef/Controllers/UtilizadorController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SweetChef.Models;
+
+namespace SweetChef.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilizadorController : ControllerBase
+    {
+        private readonly UtilizadorContext _context;
+
+        // TODO: Corrigir erro
+        /*
+        public UtilizadorController(UtilizadorContext context)
+        {
+            _context = context;
+        }
+        */
+
+        // GET: api/Utilizador
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Utilizador/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Utilizador
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Utilizador/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SweetChef/SweetChef/Models/Utilizador.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetChef.Models
+{
+    public class Utilizador
+    {
+        [Key]
+        public int Id { set; get; }
+        [Required]
+        [StringLength(60)]
+        public string Nome { set; get; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { set; get; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Pass { set; get; }
+        [DataType(DataType.Date)]
+        public string DataNascimento { set; get; }
+
+        // TODO: Adicionar coleções
+        // public virtual ICollection<Ementa> EmentaSemanal { get; set; }
+        // public virtual ICollection<Opiniao> Opiniao { get; set; }
+        // ...
+    }
+
+    public class UtilizadorContext : DbContext
+    {
+        public UtilizadorContext(DbContextOptions<UtilizadorContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO: Gerar as foreign keys.
+            /*
+            modelBuilder.Entity<Ementa>()
+                    .HasOne(t => t.utilizador)
+                    .WithMany(u => u.ementa)
+                    .HasForeignKey(t => t.user_id)
+                    .HasConstraintName("ForeignKey_Utilizador_Ementa");
+            */
+        }
+
+
+        public DbSet<Utilizador> utilizadores { get; set; }
+    }
+}
