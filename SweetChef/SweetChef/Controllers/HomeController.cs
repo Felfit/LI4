04cb1f19fe610c7108b2137dec0ae329113f80f4
Carlos Castro <cpac20.4@gmail.com>
2019-05-31 18:56:43 +0100

    Muda Receita

--- SweetChef/SweetChef/Controllers/HomeController.cs
+++ SweetChef/SweetChef/Controllers/HomeController.cs
@@ -24,7 +24,10 @@ namespace SweetChef.Controllers
         public IActionResult Index()
         {
             if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                ViewData["NomeUtilizador"] = "Utilizador";
                 return Redirect("/Home/Cozinhar/");
+            }
             return View();
         }
 
