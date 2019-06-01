9a4bacd3e8e033984cf3b03febd67b8c5608b32c
Felfit <josefernandes.fel@gmail.com>
2019-06-01 14:35:07 +0100

    Tags Get

---
+++ SweetChef/SweetChef/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SweetChef.ModelsNew;
+
+namespace SweetChef.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TagController : ControllerBase
+    {
+
+        private readonly SweetContext _context;
+
+        public TagController(SweetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Tags()
+        {
+            return Ok(_context.Tag.ToArray());
+        }
+    }
+}
